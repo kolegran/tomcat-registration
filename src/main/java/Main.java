7478cb9e95a0c20c6ea988319b22b2f1843c176a
kolegran <kolegran@gmail.com>
2019-01-24 14:33:48 +0200

    Added properties for the database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,35 @@ import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.servlet.ServletException;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws LifecycleException, ServletException, SQLException {
+        Properties properties = new Properties();
+        InputStream input;
+
+        try {
+            input = new FileInputStream("/home/zoe/projects/java-initial/tomcat-registration/app.properties");
+            properties.load(input);
+        } catch (IOException e) {
+            throw new IllegalStateException();
+        }
+        try {
+            input.close();
+        } catch (IOException e) {
+            throw new IllegalStateException();
+        }
+
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setServerName("localhost");
-        dataSource.setPortNumber(5432);
-        dataSource.setUser("postgres");
-        dataSource.setPassword("postgres");
-        dataSource.setDatabaseName("postgres");
+        dataSource.setServerName(properties.getProperty("datasource.host", "localhost"));
+        dataSource.setPortNumber(Integer.parseInt(properties.getProperty("datasource.port", String.valueOf(5432))));
+        dataSource.setUser(properties.getProperty("datasource.user", "postgres"));
+        dataSource.setPassword(properties.getProperty("datasource.password", "postgres"));
+        dataSource.setDatabaseName(properties.getProperty("datasource.databaseName", "postgres"));
         dataSource.getConnection().createStatement().execute("create table if not exists users (id bigserial primary key, firstName text, lastName text, userEmail text, username text, password text)");
 
         UserRepositoryDB userRepository = new UserRepositoryDB(dataSource);
